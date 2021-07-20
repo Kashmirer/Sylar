35a635a765b281e044a545ff7c788e67629af95f
Kashmirer <mewangbinxiao@qq.com>
2021-07-20 22:06:31 +0800

    modify

--- test.cpp
+++ test.cpp
@@ -17,6 +17,6 @@ int main(){
     int a[]={1,1,34,77,90,11,2,4,5,55};
     sort(a+1,a+10);
     for(int i=0;i<10;i++)
-        cout << a[i] << "Kashmirer";
+        cout << a[i] << "Kashmirer" <<endl;
     return 0;
 }
